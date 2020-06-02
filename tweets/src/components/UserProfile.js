185ff0481e306cb4eaf8aebe087b57a0aa5e0a6c
maria liza <mariamartinez@pursuit.org>
2020-06-02 00:00:38 -0400

    fix styling problem on userprofileinfo

--- tweets/src/components/UserProfile.js
+++ tweets/src/components/UserProfile.js
@@ -6,14 +6,13 @@ import LeftSideBar from "./LeftSideBar";
 import UserProfileInfo from "./UserProfileInfo";
 import UserPosts from "./UserPosts";
 import { AuthContext } from "../providers/AuthContext";
-import UserPosts from "./UserPosts";
+
 
 const UserProfile = () => {
   const API = apiURL();
   const [user, setUser] = useState({});
   const { token, currentUser } = useContext(AuthContext);
 
-<<<<<<< HEAD
   useEffect(() => {
     const fetchUserById = async () => {
       let res = await axios({
@@ -30,38 +29,17 @@ const UserProfile = () => {
     };
     fetchUserById();
   }, []);
-=======
-  const fetchUserById = async () => {
-    let res = await axios({
-      method: "get",
-      url: `${API}/api/users/${currentUser.uid}`,
-      headers: {
-        AuthToken: token,
-      },
-    });
-    if (res.data.user.id === currentUser.uid) {
-      return ([currentUser] = [currentUser, ...res.data.user[0]]);
-    }
-    console.log(user, "user");
-    setUser(currentUser);
-  };
-  fetchUserById();
->>>>>>> cea6169b989cd25718863991016e13d64dd13a8e
 
   return (
     <>
       <LeftSideBar />
       <UserProfileInfo />
-<<<<<<< HEAD
       <UserPosts/>
       {/* <div>
         {user.map((user) => {
           return <li key={user.id}>{user.full_name}</li>;
         })}
       </div> */}
-=======
-      <UserPosts />
->>>>>>> cea6169b989cd25718863991016e13d64dd13a8e
     </>
   );
 };
